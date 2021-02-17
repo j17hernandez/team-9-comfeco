aa530ae956bef25c53d10c197a8401da0dc33434
Felipe Bastidas <38955011+lfbastidas@users.noreply.github.com>
2021-02-16 22:13:14 -0500

    cambios del header y del footer (#6)

---
+++ components/VFooter.vue
@@ -0,0 +1,65 @@
+
+Vue.component('Footer', {
+
+
+template:`
+  <div id="app">
+  <v-app id="inspire">
+    <v-footer
+      dark
+      padless
+    >
+      <v-card
+        flat
+        tile
+        class="indigo lighten-1 white--text text-center"
+      >
+        <v-card-text>
+          <v-btn
+            v-for="icon in icons"
+            :key="icon"
+            class="mx-4 white--text"
+            icon
+          >
+            <v-icon size="24px">
+              {{ icon }}
+            </v-icon>
+          </v-btn>
+        </v-card-text>
+  
+        <v-card-text class="white--text pt-0">
+          Comfeco
+        </v-card-text>
+  
+        <v-divider></v-divider>
+  
+        <v-card-text class="white--text">
+          {{ new Date().getFullYear() }} — <strong>Team-9</strong>
+        </v-card-text>
+      </v-card>
+    </v-footer>
+  </v-app>
+</div>
+`
+
+
+})
+
+
+
+
+
+<script>
+new Vue({
+  el: '#app',
+  vuetify: new Vuetify(),
+  data: () => ({
+    icons: [
+      'mdi-facebook',
+      'mdi-twitter',
+      'mdi-linkedin',
+      'mdi-instagram',
+    ],
+  }),
+})
+</script>
